8693f7f93c1373bc5750cb2ef4cdd14ecce948ef
JohnnyPP <johnnnyppp@gmail.com>
2014-02-11 00:12:28 +0100

    Prints on the screen the solution in case Discriminant > 0

--- app/src/main/java/com/example/app/QuadraticEquationSolver.java
+++ app/src/main/java/com/example/app/QuadraticEquationSolver.java
@@ -1,7 +1,6 @@
 package com.example.app;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,11 +8,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class QuadraticEquationSolver extends ActionBarActivity {
+
+    protected float Discriminant = 0;
+    //a = 0, b = 0, c = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,18 +65,73 @@ public class QuadraticEquationSolver extends ActionBarActivity {
         }
     }
 
-    public void solveEquation(View view)
+    public String solveEquation(View view)
     {
         EditText inputTxt = (EditText) findViewById(R.id.editText);
         TextView textView = (TextView) findViewById(R.id.textViewSolution);
 
         // Store EditText in Variable
         String str = inputTxt.getText().toString();
+        String[] splitStrings = str.split(" ");
+        float a = Float.parseFloat(splitStrings[0]);
+        float b = Float.parseFloat(splitStrings[1]);
+        float c = Float.parseFloat(splitStrings[2]);
 
 
-        textView.setText("The solution " + str);
+        //textView.setText("The solution " + a + b + c);
 
+        if (a != 0)
+        {
+            Discriminant = b * b - 4 * a * c;
 
+            if (Discriminant == 0)
+            {
+                return DiscriminantEqualZero();
+            }
+
+            if (Discriminant > 0)
+            {
+                textView.setText(DiscriminantGreaterThanZero(a, b, Discriminant));
+                return DiscriminantGreaterThanZero(a, b, c);
+
+            }
+
+            if (Discriminant < 0)
+            {
+                return DiscriminantLessThanZero();
+            }
+
+            return "Quadratic equation return path";
+        }
+        else
+        {
+            return LinearEquation();
+        }
+
+    }
+
+    private String DiscriminantEqualZero()
+    {
+        return "DEZ.DiscrEqualZero(a, b, Discriminant)";
+    }
+
+    private String DiscriminantGreaterThanZero(float a, float b, float Discriminant)
+    {
+        double x1 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+        double x2 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+
+        return "The equation has two roots.\n\r" + "Discriminant: " + Discriminant
+                + "\n\rRoot1: " + x1 + "\n\r" + "Root2: " + x2;
+    }
+
+    private String DiscriminantLessThanZero()
+    {
+        return "DLTZ.DiscrLessThanZero(a, b, c, Discriminant)";
+    }
+
+    private String LinearEquation()
+    {
+        return "LE.LinEquation(b, c)";
     }
 
 }
